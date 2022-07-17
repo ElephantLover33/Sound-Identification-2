7b1b33400567da93e6c4dccaddb1818995df1778
ElephantLover33 <98704150+ElephantLover33@users.noreply.github.com>
2022-07-17 12:35:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/y95O3Mjhe/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifer.classify(gotResults);
+}
+
+function gotResults(error, results)
+{
+    console.log('got result');
+}
